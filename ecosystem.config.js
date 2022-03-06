836aa488217c8aa49d4e3158b3de1c2b0f43c1a1
fazi1live <faziislive@gmail.com>
2022-03-07 01:42:08 +0500

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,9 @@
 module.exports = {
   apps : [{
-    script: 'server.js',
-    watch: '.'
-  }, {
-    script: './server.js',
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
-  }],
-
-  deploy : {
-    production : {
-      user : 'root',
-      host : '64.227.136.195',
-      ref  : 'origin/master',
-      repo : 'https://github.com/fazi1live/Pm2-Continuous-Deployment',
-      path : '/root/Pm2-Continuous-Deployment',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      key : "/root/.ssh/deploy_keys/git_key_testing"
+    name: "testingpm2",
+    script: "server.js",
+    env: {
+      NODE_ENV: "production",
     }
-  }
-};
+  }]
+}
